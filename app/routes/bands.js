eaeb7d5b3f786ad3995a99c6aa711e7da561c2e4
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2024-03-09 14:01:21 +0000

    Refactor song model; add band model

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,4 +1,16 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
+import EmberObject from "@ember/object";
+import { A } from "@ember/array";
+
+const Band = EmberObject.extend({
+  name: "",
+});
 
 export default Route.extend({
+  model() {
+    let blackSabbath = Band.create({ name: "Black Sabbath" });
+    let metallica = Band.create({ name: "Metallica" });
+    let pearlJam = Band.create({ name: "Pearl Jam" });
+    return A([blackSabbath, metallica, pearlJam]);
+  },
 });
