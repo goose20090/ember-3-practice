038bd850321c8c1809cb13478ce7511d2f12f339
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2024-03-09 13:53:35 +0000

    Router config- and routes for bands and songs

--- app/router.js
+++ app/router.js
@@ -1,10 +1,14 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
-export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
-}
-
-Router.map(function() {
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL,
 });
+
+Router.map(function () {
+  this.route("bands");
+  this.route("songs");
+});
+
+export default Router;
